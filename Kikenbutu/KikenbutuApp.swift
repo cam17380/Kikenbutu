f36be30c45869c06975a81885f523f37987a231e
cam17380 <cam17380@ja2.so-net.ne.jp>
2022-02-24 12:01:49 +0900

    Initial Commit

---
+++ Kikenbutu/KikenbutuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KikenbutuApp.swift
+//  Kikenbutu
+//
+//  Created by dev on 2022/02/24.
+//
+
+import SwiftUI
+
+@main
+struct KikenbutuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
